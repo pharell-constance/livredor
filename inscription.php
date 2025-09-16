429d310ca4a830cc1dc200e5d0344dd6a954e069
unknown <pharellc.dev@gmail.com>
2025-09-16 09:46:49 +0200

    formulaire inscription

---
+++ inscription.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="inscription.php">
+        <div class="container">
+            <h1>Inscription</h1>
+            <p>Remplissez ce formulaire pour créer un compte</p>
+            <hr>
+            <label for="user"><b>Pseudo</b></label>
+            <imput type = "text" placeholder = "entrez votre pseudo" name = "pseudo" id = "pseudo" required />
+            
+
+        </div>
+    </form>
+</body>
+</html>
